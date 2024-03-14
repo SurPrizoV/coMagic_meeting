b31305f71e326a69e9ccdee0e585609fa407e417
Роман Призов <roman_prizov@MacBook-Air-Roman.local>
2024-03-14 22:24:34 +0300

    изменил пути

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AppRoutes } from "./services/routes";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppRoutes />
-  </BrowserRouter>
+  </HashRouter>
 );
